a37ebcbe04bdc99c1855ae87f197fff09facf62c
collin <collinzrj@outlook.com>
2020-02-15 16:01:19 -0500

    Initial Commit

---
+++ MeasureWriteSpeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeasureWriteSpeed
+//
+//  Created by 张睿杰 on 2020/2/15.
+//  Copyright © 2020 张睿杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
